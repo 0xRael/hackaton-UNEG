83762b98d6ddbd96f4949c2e262b4649ab550301
rael5 <rael1up@gmail.com>
2025-07-22 18:14:16 -0400

    Catalogo servicios + Servidor base flask

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+@app.route("/catalogo")
+def catalogo():
+	return render_template('catalogo.html')
+
+@app.route("/registro")
+def registro():
+	return render_template('registro.html')
+
+@app.route("/login")
+def login():
+	return render_template('login.html')
+
+if __name__ == '__main__':  
+   app.run(debug=True)  
